b3cca91e06986d39d90770054fdeeb0920b6b493
Sandeep Kumar <kamboz629@gmail.com>
2025-02-09 17:05:03 +0530

    journey

---
+++ app.js
@@ -0,0 +1,20 @@
+// <!-- <div id="parent">
+// <div id="child">
+//   <h1>i am h1 tag </h1>
+
+// </div>
+// </div> -->
+// const parent = React.createElement(
+//     "div",
+//     { id: "parent" },
+//     React.createElement(
+//         "ul",
+//         { id: "child" },
+//         [React.createElement(
+//             "li",
+//             {}, "student services "),
+//         React.createElement(
+//             "li",
+//             {}, "Acadmecis")]));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
